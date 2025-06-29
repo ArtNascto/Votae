0522e16b03b34924f796d70705ca2d70d776bace
ArtNascto <arthurgnascto@gmail.com>
2025-06-29 20:29:34 -0300

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Votae;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Votae;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
